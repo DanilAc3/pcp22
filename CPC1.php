ce75c62d79203eece6a1b86c818f7e181130677e
DanilAc3 <56014103+DanilAc3@users.noreply.github.com>
2019-10-01 10:09:12 +0500

    Add files via upload

---
+++ CPC1.php
@@ -0,0 +1,37 @@
+<?php
+$a = a;
+echo a;
+
+//
+$a = 10;
+$b = 2;
+$result = $a+$b;
+echo $result;
+$result = $a-$b;
+echo result;
+$result = $a/$b;
+echo $result;
+$result = $a*$b;
+echo $result;
+
+//
+$c = 15;
+$d = 2;
+$result = $c+$d;
+echo $result;
+
+//
+$a = 10;
+$b = 2;
+$c = 5;
+$result = ($a + $b+ $c / 3);
+echo $result;
+
+//
+$a = 17;
+$b = 10;
+$c = $a - $b;
+echo $c;
+$d = 17;
+$result = $c + $d;
+echo $result;
---
+++ CPC3.php
@@ -0,0 +1,11 @@
+<?php
+$arr = ['a' , 'b' , 'c'];
+echo var_dump($arr);
+
+//
+echo $arr[0] . $arr[1] . $arr[2];
+
+//
+$arr = [2, 5, 3, 9];
+$result = $arr [0] * $arr [1] + $arr [2] * $arr[3];
+echo $result;
